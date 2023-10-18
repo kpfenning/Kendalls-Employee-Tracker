f59b4fb70946133fa3a10228750577115417d929
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2023-10-18 16:31:31 -0700

    created tables, added to seed file, work in server

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id),
+    PRIMARY KEY (id),
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
+    PRIMARY KEY (id),
+    ON DELETE SET NULL
+)
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+
+
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -14,5 +12,64 @@ const db = mysql.createConnection({
     password: "Wildcats123!",
     database: "employee_tracker_db"
 },
-    console.log(`Connected to the employee_tracker_db database.`)
+    console.log(`Connected to the employee_tracker_db database.`),
+
+    optionPrompt()
 );
+
+
+function optionPrompt(){
+    inquirer.prompt([
+    {
+        type: 'list',
+        name: 'userChoice',
+        message: 'What would you like to do?',
+        choices: [
+            "View All Departments.",
+            "View All Roles.",
+            "View All Employees.",
+            "Add a Department.",
+            "Add a Role.",
+            "Add an Employee.",
+            "Update an Employee Role.",
+            "Exit"
+        ]
+    }
+    ]).then((res) =>{
+        console.log(res.userChoice);
+        switch(res.userChoice){
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add a Department':
+                addDepartment();
+                break;
+            case 'Add a Role':
+                addRole();
+                break;
+            case 'Add an Employee':
+                addEmployee();
+                break;
+            case 'Update an Employee Role':
+                updateRole();
+                break;
+            case 'Exit':
+                createConnection.end();
+                break;
+        }
+
+    }).catch((err) => {
+        if(err)throw err;
+    });
+}
+
+function viewAllDepartments(){
+    let query =
+    ``
+}
